92023beafbf9448117022dcffa0f44b0064a0d39
张鹏 <scbeta@qq.com>
2023-04-10 19:07:01 +0800

    Remove parameter userId.

--- src/Hikvision.NetSDK.Api/Service/VideoFileService.cs
+++ src/Hikvision.NetSDK.Api/Service/VideoFileService.cs
@@ -26,9 +26,9 @@ namespace Hikvision.NetSDK.Api.Service
             return downloadHandle;
         }
 
-        public virtual int StartDownloadFile(int userId, string remoteFile, string destinationPath)
+        public virtual int StartDownloadFile(string remoteFile, string destinationPath)
         {
-            int downloadHandle = Invoke(NET_DVR_GetFileByName(userId, remoteFile, destinationPath));
+            int downloadHandle = Invoke(NET_DVR_GetFileByName(session.UserId, remoteFile, destinationPath));
             uint iOutValue = 0;
             Invoke(NET_DVR_PlayBackControl_V40(downloadHandle, NET_DVR_PLAYSTART, IntPtr.Zero, 0, IntPtr.Zero, ref iOutValue));
             return downloadHandle;
